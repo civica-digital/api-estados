ca73298f49ace4ff1e0de4e8f83402593f42228c
mikesaurio <mikesaurio@gmail.com>
2017-02-06 11:54:23 -0600

    se recibe csv

--- api/lib/data.ex
+++ api/lib/data.ex
@@ -1,10 +1,10 @@
 defmodule Data do
-  
+  #Data.load_csv
   def load_csv do
-    File.stream!(Path.expand("lib/estados.csv")) |>
-    CSV.decode(separator: ?\t) |>
-    Enum.map(fn row ->
-        Enum.map(row, &String.upcase/1)
-    end)
+    m = File.stream!(Path.expand("lib/estados.csv")) |> CSV.decode(separator: ?\t, headers: true) 
+    Enum.map(m, 
+        fn(x) -> 
+           x
+    end) 
   end
 end
