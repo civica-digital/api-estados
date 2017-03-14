1dc7d01d8b9a918306bc872a3d980af7d93fb542
emanuelzh <emanuelz@emanuelz.com.mx>
2017-03-14 02:04:24 -0600

    Ejecuta la carga de datos desde el seeder

--- api/priv/repo/seeds.exs
+++ api/priv/repo/seeds.exs
@@ -16,8 +16,12 @@ alias Api.Constituency
 
 alias Api.Repo
 
+#Eliminar data
 Repo.delete_all(Constituency)
 Repo.delete_all(Town)
 Repo.delete_all(State)
 
-
+#Insertar data
+Data.load_csv("estados")
+Data.load_csv("municipios")
+Data.load_csv("secciones")
