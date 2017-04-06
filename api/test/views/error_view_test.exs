07f3e9fa12bddeaa2c2a7358d21b0a3e7643edea
mikesaurio <mikesaurio@gmail.com>
2017-04-06 11:45:20 -0500

    se arreglan tests

--- api/test/views/error_view_test.exs
+++ api/test/views/error_view_test.exs
@@ -6,16 +6,16 @@ defmodule Api.ErrorViewTest do
 
   test "renders 404.json" do
     assert render(Api.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+           %{errors: %{detail: "Página no encontrada, para la documentación completa visita: /api/swagger/#/"}}
   end
 
   test "render 500.json" do
     assert render(Api.ErrorView, "500.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{errors: %{detail: "Error servidor interno, para la documentación completa visita: /api/swagger/#/"}}
   end
 
   test "render any other" do
     assert render(Api.ErrorView, "505.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{errors: %{detail: "Error servidor interno, para la documentación completa visita: /api/swagger/#/"}}
   end
 end
